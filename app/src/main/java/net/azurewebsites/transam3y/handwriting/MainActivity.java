e472cc76cb88d9e6bc45e788de1e67d022edeb0c
zyouyowa <transam33y@gmail.com>
2015-04-05 18:29:10 +0900

    set 2 buttons and a SurfaceView. but i dont know how to use SurfaceView.

--- app/src/main/java/net/azurewebsites/transam3y/handwriting/MainActivity.java
+++ app/src/main/java/net/azurewebsites/transam3y/handwriting/MainActivity.java
@@ -1,10 +1,19 @@
 package net.azurewebsites.transam3y.handwriting;
 
+import android.app.ActionBar;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,8 +21,32 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
+    public class DrawSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+        private SurfaceHolder mSurfaceHolder;
+
+        public DrawSurfaceView(Context context) {
+            super(context);
+            mSurfaceHolder = getHolder();
+            mSurfaceHolder.setFormat(PixelFormat.TRANSLUCENT);
+            getHolder().addCallback(this);
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
