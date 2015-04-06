c20dca54b4b492161fb6ce30bf38a5f809c078d8
zyouyowa <transam33y@gmail.com>
2015-04-06 10:32:06 +0900

    test commit

--- app/src/main/java/net/azurewebsites/transam3y/handwriting/MainActivity.java
+++ app/src/main/java/net/azurewebsites/transam3y/handwriting/MainActivity.java
@@ -1,19 +1,13 @@
 package net.azurewebsites.transam3y.handwriting;
 
-import android.app.ActionBar;
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 public class MainActivity extends ActionBarActivity {
 
